9eb82fd6448f8a014f263d922ff561fdc7017dd5
tubbjr <missionbit@missions-MacBook-Air.local>
2016-03-22 17:53:08 -0700

    woowo
    asdfhgjhj

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -6,15 +6,15 @@ demo.state0 = function(){};
 
 demo.state0.prototype = {
 	preload: function(){
-		game.load.image('trees', 'assets/background/treeBG.png');
-//		game.load.spritesheet('adam', 'assets/spritesheets/adamSpritesheet.png', 240, 370);
+		game.load.image('dojo', 'assets/image00.png');
+		game.load.spritesheet('ajay', 'assets/image01.png', 110, 155);
 	},
 
 	create: function(){
 //        game.world.setBounds(0,0, 2813, 1000);
 //        game.physics.startSystem(Phaser.Physics.ARCADE);
-//        var tree = game.add.sprite(0, 0, 'trees');
-//        adam = game.add.sprite(0, 450, 'adam');
+        var tree = game.add.sprite(0, 0, 'dojo');
+        adam = game.add.sprite(0, 450, 'ajay');
 //        adam.animations.add('walk', [0, 1 , 2 , 3 , 4]);
 //        adam.anchor.setTo(0.5, 0.5);
 //        adam.scale.setTo(0.7, 0.7);
