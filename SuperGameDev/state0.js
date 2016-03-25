be2abae5a7eac7a90ed956745f929b9f24df1941
tubbjr <missionbit@missions-MacBook-Air.local>
2016-03-24 20:10:21 -0700

    hehhed
    dfgb

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -1,6 +1,7 @@
 var demo = {};
 var speed = 6;
 var adam;
+var old;
 demo.state0 = function(){};
 
 
@@ -13,12 +14,13 @@ demo.state0.prototype = {
 	},
 
 	create: function(){
-        game.world.setBounds(0,0, 1500, 500);
+        game.world.setBounds(0,0, 1500, 550);
 //        game.physics.startSystem(Phaser.Physics.ARCADE);
         var tree = game.add.sprite(0, 0, 'dojo');
         adam = game.add.sprite(0, 450, 'ajay');
 //        adam.frame = ;
         adam.animations.add('walk', [0, 1 , 2 , 3 , 4]);
+        old = game.add.sprite(0, 450, 'old');
         
         tree.height=game.height;
         tree.width=game.width;
@@ -28,7 +30,7 @@ demo.state0.prototype = {
         game.physics.enable(adam);
         adam.body.collideWorldBounds = true;
 //        
-//        game.camera.follow(adam);
+        game.camera.follow(adam);
 //        
 //        game.scale.scaleMODE = Phaser.ScaleManager.SHOW_ALL;
 	},
