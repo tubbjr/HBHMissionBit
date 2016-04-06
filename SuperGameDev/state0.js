b911075a6299c435f078cb6fcfaad9f26f1044d6
tubbjr <missionbit@missions-MacBook-Air.local>
2016-04-05 17:24:58 -0700

    fduo
    skl

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -1,7 +1,8 @@
 var demo = {};
 var speed = 6;
 var adam;
-var old;
+//var old;
+var carrot;
 demo.state0 = function(){};
 
 
@@ -9,7 +10,8 @@ demo.state0.prototype = {
 	preload: function(){
 		game.load.image('dojo', 'assets/image00.png');
 		game.load.spritesheet('ajay', 'assets/ajay.png', 133, 211);
-        game.load.spritesheet('old', 'assets/sampepper.png', 850, 1107);
+//        game.load.spritesheet('old', 'assets/sampepper.png', 850, 1107);
+        game.load.spritesheet('carrot', 'assets/carrot.png', 1400, 1400);
 
 	},
 
@@ -18,16 +20,18 @@ demo.state0.prototype = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         var tree = game.add.sprite(0, 0, 'dojo');
         adam = game.add.sprite(0, 450, 'ajay');
-        old = game.add.sprite(300, 350, 'old');
+//        old = game.add.sprite(300, 350, 'old');
 //        adam.frame = ;
         adam.animations.add('walk', [0, 1 , 2 , 3 , 4]);
+        carrot = game.add.sprite(300, 250, 'carrot');
        
         
         
         tree.height=game.height;
         tree.width=game.width;
 //        adam.anchor.setTo(0.5, 0.5);
-        old.scale.setTo(0.18, 0.18);
+//        old.scale.setTo(0.18, 0.18);
+        carrot.scale.setTo(0.4, 0.4)
 //        
         game.physics.enable(adam);
         adam.body.collideWorldBounds = true;
@@ -60,7 +64,7 @@ demo.state0.prototype = {
                     if(adam.y < 400){
                         adam.y = 400;
                     }
-//            if (game.input.keyboard.isDown(Phaser.Keyboard.))
+//            if (game.input.keyboard.isDown(Phaser.Keyboard.J))
         }
 	}
 
