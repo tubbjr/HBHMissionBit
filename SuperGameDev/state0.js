9037d63070a0b60f33fa77d32d957c24cd10e3fe
sam <sam442000@gmail>
2016-04-12 16:51:13 -0700

    zdxhgfcjhgvkb
    gvhjbkn

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -1,6 +1,7 @@
 var demo = {};
 var speed = 6;
-var adam;
+
+var old;
 demo.state0 = function(){};
 
 
@@ -8,33 +9,46 @@ demo.state0.prototype = {
 	preload: function(){
 		game.load.image('dojo', 'assets/image00.png');
 		game.load.spritesheet('ajay', 'assets/ajay.png', 133, 211);
+        game.load.spritesheet('old', 'assets/sampepper.png', 850, 1107);
+
 	},
 
 	create: function(){
-//        game.world.setBounds(0,0, 2813, 1000);
-//        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.setBounds(0,0, 1500, 550);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         var tree = game.add.sprite(0, 0, 'dojo');
         adam = game.add.sprite(0, 450, 'ajay');
+        old = game.add.sprite(300, 350, 'old');
 //        adam.frame = ;
         adam.animations.add('walk', [0, 1 , 2 , 3 , 4]);
+       
+        
+        
+        tree.height=game.height;
+        tree.width=game.width;
 //        adam.anchor.setTo(0.5, 0.5);
-//        adam.scale.setTo(0.7, 0.7);
+        old.scale.setTo(0.18, 0.18);
 //        
-//        game.physics.enable(adam);
-//        adam.body.collideWorldBounds = true;
+        game.physics.enable(adam);
+        adam.body.collideWorldBounds = true;
 //        
-//        game.camera.follow(adam);
+        game.camera.follow(adam);
 //        
 //        game.scale.scaleMODE = Phaser.ScaleManager.SHOW_ALL;
+		
+		function ballHitBrick (_adam, _old) {
+
+    _old.kill();
+
 	},
 
 	update: function(){
-        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        if (game.input.keyboard.isDown(Phaser.Keyboard.D)){
                               adam.x =  adam.x + speed; 
                             adam.animations.play('walk', 20, true);
                                adam.scale.setTo(-0.7, 0.7)
         }
-        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.A)){
             adam.x = adam.x - speed;
               adam.animations.play('walk', 20, true);
             adam.scale.setTo(0.7, 0.7)
@@ -42,7 +56,7 @@ demo.state0.prototype = {
          else{
         adam.animations.stop('walk');
         }
-       if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+       if (game.input.keyboard.isDown(Phaser.Keyboard.S)){
             adam.y = adam.y + speed;
         }
        
@@ -51,6 +65,7 @@ demo.state0.prototype = {
                     if(adam.y < 400){
                         adam.y = 400;
                     }
+//            if (game.input.keyboard.isDown(Phaser.Keyboard.))
         }
 	}
 
