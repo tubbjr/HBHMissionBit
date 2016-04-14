783c7286282a85dce17f5b82204f7be56e171a28
Champagne1245 <Rodriguezjulio150@gmail.com>
2016-04-14 16:01:17 -0700

    coolc
    effefe2f

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -65,3 +65,58 @@ demo.state0.prototype = {
 	}
 
 };
+var content = [
+    "cool succ",
+    "photon storm presents",
+    "a phaser production",
+    " ",
+    "Kern of Duty",
+    " ",
+    "directed by rich davey",
+    "rendering by mat groves",
+    "    ",
+    "03:45, November 4th, 2014",
+    "somewhere in the north pacific",
+    "mission control bravo ...",
+];
+
+var text;
+var index = 0;
+var line = '';
+
+function create() {
+
+1
+    text = game.add.text(32, 380, '', { font: "30pt Courier", fill: "#19cb65", stroke: "#119f4e", strokeThickness: 2 });
+
+    nextLine();
+
+}
+
+function updateLine() {
+
+    if (line.length < content[index].length)
+    {
+        line = content[index].substr(0, line.length + 1);
+        // text.text = line;
+        text.setText(line);
+    }
+    else
+    {
+        //  Wait 2 seconds then start a new line
+        game.time.events.add(Phaser.Timer.SECOND * 2, nextLine, this);
+    }
+
+}
+
+function nextLine() {
+
+    index++;
+
+    if (index < content.length)
+    {
+        line = '';
+        game.time.events.repeat(80, content[index].length + 1, updateLine, this);
+    }
+
+}
