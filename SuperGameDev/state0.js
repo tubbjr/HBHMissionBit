e4abcb275112f254421e301abb57cb2c00d34e3d
Champagne1245 <Rodriguezjulio150@gmail.com>
2016-04-14 16:15:59 -0700

    committing

--- SuperGameDev/state0.js
+++ SuperGameDev/state0.js
@@ -23,15 +23,34 @@ demo.state0.prototype = {
 	create: function(){
         game.world.setBounds(0,0, 1500, 550);
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+//        enemies = game.add.group();
+//        enemies.enableBody = true;
+        
+//        var enemy = game.add.sprite(100, 100, 'enemy');
+//        var enemy1 = enemies.create(100, 450, 'enemy');
+        
         var tree = game.add.sprite(0, 0, 'dojo');
-        adam = game.add.sprite(0, 250, 'ajay');
+//<<<<<<< HEAD
+//        adam = game.add.sprite(0, 450, 'ajay');
+        adam = game.add.sprite(0,450,'test');
+        adam.scale.setTo(1.5, 1.5);
+        
+        
+//=======
+//        adam = game.add.sprite(0, 250, 'ajay');
+//>>>>>>> master
 //        old = game.add.sprite(300, 350, 'old');
 //        adam.frame = ;
-        adam.animations.add('walk', [0, 1 , 2 , 3 , 4]);
+        adam.animations.add('walk', [0,1,2,3,4,5]);
+//        adam.animations.currentAnim.speed = 10;
         
         var carrot = game.add.sprite(0, 150, 'carrot');
         
-       
+//       var enemy = game.add.sprite(50, 150, 'enemy');
+//        enemy.scale.setTo(1.6,1.6);
+        
+//        enemy.animations.add('move')
         
         
         tree.height=game.height;
@@ -40,106 +59,107 @@ demo.state0.prototype = {
 //        old.scale.setTo(0.18, 0.18);
         carrot.scale.setTo(0.6, 0.6)
 //        
-        game.physics.enable(adam);
+        game.physics.arcade.enable(adam);
+        adam.body.bounce.y = 0.2;
+        adam.body.gravity.y = 600;
         adam.body.collideWorldBounds = true;
-//        
         game.camera.follow(adam);
+//        this.adam.body.gravity.y = 1000;
+        cursors = game.input.keyboard.createCursorKeys();
         
-        cursors =game.input.keyboard.createCursorKeys();
-        
-    
-//        
-//        game.scale.scaleMODE = Phaser.ScaleManager.SHOW_ALL;
 	},
 
 	update: function(){
-        adam.body.velocity.x = 0;
-        if (cursors.right.isDown)){
-//                              adam.x =  adam.x + speed;
-                        adam.body.velocity.x = 150;
+       adam.body.velocity.x = 0;
 
-          
-                            adam.animations.play('walk', 20, true);
-                               adam.scale.setTo(-0.7, 0.7);
-        }
-<<<<<<< HEAD
-        else if (game.input.keyboard.isDown(Phaser.Keyboard.A)){
-            adam.x = adam.x - speed;
-              adam.animations.play('walk', 20, true);
-            adam.scale.setTo(0.7, 0.7)
-        }
-         else{
-        adam.animations.stop('walk');
-        }
-       if (game.input.keyboard.isDown(Phaser.Keyboard.S)){
-            adam.y = adam.y + speed;
-        }
-       
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            adam.y = adam.y - speed;
-                    if(adam.y < 400){
-                        adam.y = 400;
-                    }
-//            if (game.input.keyboard.isDown(Phaser.Keyboard.))
-        }
-	}
-
-};
-var content = [
-    "cool succ",
-    "photon storm presents",
-    "a phaser production",
-    " ",
-    "Kern of Duty",
-    " ",
-    "directed by rich davey",
-    "rendering by mat groves",
-    "    ",
-    "03:45, November 4th, 2014",
-    "somewhere in the north pacific",
-    "mission control bravo ...",
-];
-
-var text;
-var index = 0;
-var line = '';
-
-function create() {
-
-1
-    text = game.add.text(32, 380, '', { font: "30pt Courier", fill: "#19cb65", stroke: "#119f4e", strokeThickness: 2 });
-
-    nextLine();
-
-}
-
-function updateLine() {
-
-    if (line.length < content[index].length)
+    if (cursors.left.isDown)
     {
-        line = content[index].substr(0, line.length + 1);
-        // text.text = line;
-        text.setText(line);
+        //  Move to the left
+        adam.body.velocity.x = -120;
+        adam.scale.setTo(-1.5,1.5);
+        adam.animations.play('walk', 10, true);
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+       adam.body.velocity.x = 150;
+        adam.scale.setTo(1.5,1.5);
+        adam.animations.play('walk', 10, true);
     }
     else
     {
-        //  Wait 2 seconds then start a new line
-        game.time.events.add(Phaser.Timer.SECOND * 2, nextLine, this);
+        //  Stand still
+        adam.animations.stop();
+
+        adam.frame = 0;
     }
 
-}
-
-function nextLine() {
-
-    index++;
-
-    if (index < content.length)
+    if(cursors.down.isDown){
+        adam.body.velocity.y = 350;
+    }
+    //  Allow the player to jump
+    if (cursors.up.isDown)
     {
-        line = '';
-        game.time.events.repeat(80, content[index].length + 1, updateLine, this);
+        adam.body.velocity.y = -350;
     }
+	},
 
-}
+};
+//var content = [
+//    "cool succ",
+//    "photon storm presents",
+//    "a phaser production",
+//    " ",
+//    "Kern of Duty",
+//    " ",
+//    "directed by rich davey",
+//    "rendering by mat groves",
+//    "    ",
+//    "03:45, November 4th, 2014",
+//    "somewhere in the north pacific",
+//    "mission control bravo ...",
+//];
+//
+//var text;
+//var index = 0;
+//var line = '';
+//
+//function create() {
+//
+//1
+//    text = game.add.text(32, 380, '', { font: "30pt Courier", fill: "#19cb65", stroke: "#119f4e", strokeThickness: 2 });
+//
+//    nextLine();
+//
+//}
+//
+//function updateLine() {
+//
+//    if (line.length < content[index].length)
+//    {
+//        line = content[index].substr(0, line.length + 1);
+//        // text.text = line;
+//        text.setText(line);
+//    }
+//    else
+//    {
+//        //  Wait 2 seconds then start a new line
+//        game.time.events.add(Phaser.Timer.SECOND * 2, nextLine, this);
+//    }
+//
+//}
+//
+//function nextLine() {
+//
+//    index++;
+//
+//    if (index < content.length)
+//    {
+//        line = '';
+//        game.time.events.repeat(80, content[index].length + 1, updateLine, this);
+//    }
+//
+//}
 =======
 //        else if (cursors.left.isDown)){
 ////            adam.x = adam.x - speed;
